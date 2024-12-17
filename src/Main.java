75ed9deeaea9ef030fd2b08aaa3ee8c36a5e6aa4
SnehalMarutiSalokhe <snehal.maruti.salokhe@gmail.com>
2024-12-17 10:58:18 +0530

    implemented: Dice roll functionality to generate a random number between 1 to 6.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
-
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         int startPosition = 0;
         System.out.println("Welcome to Snake and Ladder Game!");
         System.out.println("Starting position of the player: " + startPosition);
+
+        Random random = new Random();
+        int dieRoll = random.nextInt(6) + 1;
+        System.out.println("Die rolled: " + dieRoll);
     }
 }
 
