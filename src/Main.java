cd2f742089bb1fe337e89c72a9cc30a3c94bc555
SnehalMarutiSalokhe <snehal.maruti.salokhe@gmail.com>
2024-12-17 12:20:09 +0530

    implemented: Two-player gameplay with turn-based logic and winner determination

--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,55 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int position = 0 , startPosition=0;
-        int diceRolls = 0;
-        System.out.println("Welcome to Snake and Ladder Game!");
-        System.out.println("Starting position of the player: " + startPosition);
+        int player1Position = 0, player2Position = 0, diceRolls = 0;
 
-        Random random = new Random();
+        boolean isPlayer1Turn = true;
         final int WINNING_POSITION = 100;
 
-        while (position < WINNING_POSITION) {
+        Random random = new Random();
+
+        System.out.println("Welcome to Snake and Ladder Game!");
+        System.out.println("Starting position of the Both players: " + player2Position);
+
+
+        while (player1Position < WINNING_POSITION && player2Position < WINNING_POSITION) {
             diceRolls++;
             int dieRoll = random.nextInt(6) + 1;
             int option = random.nextInt(3);
 
-            switch (option) {
-                case 0:
-                    break;
-                case 1:
-                    if (position + dieRoll <= WINNING_POSITION)
-                    position += dieRoll;
-                    break;
-                case 2:
-                    position -= dieRoll;
-                    if (position < 0) position = 0;
-                    break;
+            if (isPlayer1Turn) {
+                player1Position = updatePosition(player1Position, dieRoll, option, WINNING_POSITION);
+                System.out.println("Player 1: Die = " + dieRoll + ", Position = " + player1Position);
+                if (option == 1) isPlayer1Turn = true;
+                player2Position = updatePosition(player2Position, dieRoll, option, WINNING_POSITION);
+                System.out.println("Player 2: Die = " + dieRoll + ", Position = " + player2Position);
+                if (option == 1) isPlayer1Turn = false;
             }
-            System.out.println("Dice roll " + diceRolls + ": Die = "
-                    + dieRoll + ", Position = " + position);
+            isPlayer1Turn = !isPlayer1Turn;
         }
-        System.out.println("Player has won the game!");
+        if (player1Position == WINNING_POSITION) {
+            System.out.println("Player 1 wins!");
+        } else {
+            System.out.println("Player 2 wins!");
         }
+    }
 
+    private static int updatePosition(int position, int dieRoll, int option, int winningPosition) {
+        switch (option) {
+            case 0:
+                break;
+            case 1:
+                if (position + dieRoll <= winningPosition) {
+                    position += dieRoll; // Ladder
+                }
+                break;
+            case 2:
+                position -= dieRoll; // Snake
+                if (position < 0) position = 0;
+                break;
+        }
+        return position;
+
+    }
 }
 
